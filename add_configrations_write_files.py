d95e23bc8bdfd941527132cee47688cead21dc27
Moa Degerfält <40612205+MoaDegerfalt@users.noreply.github.com>
2024-06-22 17:07:25 +0200

    index-add_configrations_write_files.py
    A fix for the indexes and change of the jobscript making

--- add_configrations_write_files.py
+++ add_configrations_write_files.py
@@ -38,20 +38,22 @@ def generate_combinations(hyperparameters):
 
 
 # Kollar om kombinationen redan körts, om inte så sapas en config fil och ett jobscript.
-def add_combination_if_not_exists(new_combination, filename, index, name, template):
+def add_combination_if_not_exists(new_combination, filename, index, name, folder_name, template):
     done_combinations = load_done_combinations(filename)  # Läser in json fil med alla befintliga kombinationer.
     comb_hash = create_hash(new_combination)  # Generera en unik hash för den ny a kombinationen.
     if comb_hash in done_combinations:  # Kolla om konbinationen redan finns
-        print("Kombinationen finns redan. \n" )
+        print("Kombinationen finns redan. \n")
+        return index
     else:
-        write_files(new_combination, name, index, "jobbsctipts", "configuration_files", template)
+        write_files(new_combination, name, index, folder_name, template)
         done_combinations[comb_hash] = new_combination
         save_combinations(filename, done_combinations)
         print("Kombinationen har lagts till.\n")
+        return index + 1
 
 
 # Writes new configuration files from a template and saves the files in a folder. Creates jobscripts for each configuration.
-def write_files(combination, name, index, jobbscript_folder, configurations_folder, template):
+def write_files(combination, name, index, configurations_folder, template):
     # Write configuration file
     filename_conf = f"{name}_{index}.yaml"
     if not os.path.exists(configurations_folder):
@@ -66,43 +68,42 @@ def write_files(combination, name, index, jobbscript_folder, configurations_fold
         f2.write("\n")
         yaml.dump(combination, f2)
 
-    # Write the jobscript
-    if not os.path.exists(jobbscript_folder):
-        os.makedirs(jobbscript_folder)
-    filename_jobb = 'jobscript_' + str(name) + '_' + str(index)
-    output_path_jobb = os.path.join(jobbscript_folder, filename_jobb)
+    return output_path_config
 
-    with open(output_path_jobb, 'w') as f:
+def main(hyperparameters, filename, name, folder_name,template):
+    # Hyperparameters är en dictionary med vilka hyperparametrar som ska varieras och en lista med de värden som ska testas.
+    # Filename är en json fil med de parametrar som redan körts.
+    # Name är det önskade namnet på svepet, här är det viktigt att välja ett unikt namn så at inte körningarna blandas ihop i wandbai
+    # Template är en .yaml som configurationsfilerna ska utgå ifrån.
+
+    i = 1
+    for combinations in generate_combinations(hyperparameters):
+        i = add_combination_if_not_exists(combinations, filename, i, name, folder_name, template)
+
+    filename_JOB = "jobscript_" + name
+    with open(filename_JOB, 'w') as f:
         f.write('#!/usr/bin/env bash\n')
         f.write('#SBATCH -A NAISS2023-3-31 -p alvis\n')
         f.write('#SBATCH -N 1 --gpus-per-node=T4:1 \n')
         f.write('#SBATCH -t 0-02:00:00\n\n\n')
         f.write(
-            'apptainer exec nequip.sif nequip-train ' + "/mimer/NOBACKUP/groups/ltu-fy/Moa/" + configurations_folder + "/" + filename_conf + '\n')
+            'apptainer exec nequip.sif nequip-train ' + "/mimer/NOBACKUP/groups/ltu-fy/Moa/" + folder_name + "/" + name + "${_$SLURM_ARRAY_TASK_ID}" + ".yaml" + '\n')
 
 
-def main(hyperparameters, filename, name, template):
-    # Hyperparameters är en dictionary med vilka hyperparametrar som ska varieras och en lista med de värden som ska testas.
-    # Filename är en json fil med de parametrar som redan körts.
-    # Name är det önskade namnet på svepet, här är det viktigt att välja ett unikt namn så at inte körningarna blandas ihop i wandbai
-    # Template är en .yaml som configurationsfilerna ska utgå ifrån.
-    i = 1
-    for combinations in generate_combinations(hyperparameters):
-        add_combination_if_not_exists(combinations, filename, i, name, template)
-        i = i + 1
 
-
-hyperparameters = {
-    "invariant_layers": {"values": [4, 3, 2, 1]},
-    "invariant_neurons": {"values": [128, 96, 64, 32]},
-    "num_layers": {"values": [10, 8, 6, 4, 3, 2, 1]},
-    "parity": {"values": [True, False]},
-    "l_max": {"values": [3, 2, 1]},
-    "num_features": {"values": [40, 30, 20, 10]},  
-    "num_basis": {"values": [10, 8, 6, 4]},
+hyperparameters_r_max = {
+    "invariant_layers": {"values": [2, 1]},
+    "invariant_neurons": {"values": [32]},
+    "num_layers": {"values": [6, 5, 4, 3, 2]},
+    "parity": {"values": [True]},
+    "l_max": {"values": [3, 2]},
+    "num_features": {"values": [32, 16]},
+    "num_basis": {"values": [10, 8]},
     "use_sc": {"values": [True]},
+    "r_max": {"values": [5.5, 4.5, 4, 3.5, 3]},
 }
 
-filename = "hyperparameter_combinations_2.json"
+filename = "hyperparameter_combinations_r_max.json"
+folder_name = "configuration_files"
 
-main(hyperparameters, filename, "sweep_test", "Base.yaml")
+main(hyperparameters_r_max, filename, "sweep_r_max", folder_name,"Base_r_max.yaml")
